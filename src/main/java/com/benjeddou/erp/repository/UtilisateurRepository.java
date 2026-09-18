e9e8647b360907c5a3cd09b76ef7f14b8043f353
sirinerh03 <sirine.rhaiem@esprit.tn>
2026-09-18 13:11:36 +0100

    new corrections

--- src/main/java/com/benjeddou/erp/repository/UtilisateurRepository.java
+++ src/main/java/com/benjeddou/erp/repository/UtilisateurRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+import java.util.List;
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
     Optional<Utilisateur> findByNomUtilisateur(String nomUtilisateur);
